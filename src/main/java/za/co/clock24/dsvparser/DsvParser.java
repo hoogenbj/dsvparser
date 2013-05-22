dc913862aa785b9ba2b8477c0c243f62ba798084
Johan Hoogenboezem <hoogenbj@worldonline.co.za>
2013-05-22 11:33:06 +0200

    Add javadoc

--- src/main/java/za/co/clock24/dsvparser/DsvParser.java
+++ src/main/java/za/co/clock24/dsvparser/DsvParser.java
@@ -50,25 +50,56 @@ public class DsvParser<T> {
 	private DsvFieldCallback fieldCallback;
 	private DsvRecordCallback<T> recordCallback;
 
+	/**
+	 * Set a callback object that will be invoked after every field is processed. 
+	 * 
+	 * @param fieldCallback
+	 */
 	public void setFieldCallback(DsvFieldCallback fieldCallback) {
 		this.fieldCallback = fieldCallback;
 	}
 
+	/**
+	 * Set a callback object that will be invoked after every record/line is processed,
+	 * but before the line/record is added to the results.
+	 *  
+	 * @param recordCallback
+	 */
 	public void setRecordCallback(DsvRecordCallback<T> recordCallback) {
 		this.recordCallback = recordCallback;
 	}
 
+	/**
+	 * If you use the default parser implementation, this is the record parser which will be used.
+	 * 
+	 * @author johan
+	 *
+	 */
 	public static class DefaultRecordParser implements DsvRecordParser<String[]> {
 		public String[] parseRecord(String[] fields) {
 			return fields;
 		}
 	}
 
+	/**
+	 * A convenience method for constructing and returning a default parser implementation
+	 * which parses the fields as an array of strings.
+	 * 
+	 * @param reader
+	 * @return
+	 */
 	public static DsvParser<String[]> createWithDefaultParser(Reader reader) {
 		DsvParser<String[]> parser = new DsvParser<String[]>(reader, new DefaultRecordParser());
 		return parser;
 	}
 	
+	/**
+	 * If you want to specify your own record parser to provide you with read-made records
+	 * in the results, then use this constructor.
+	 * 
+	 * @param reader
+	 * @param parser
+	 */
 	public DsvParser(Reader reader, DsvRecordParser<T> parser) {
 		if (!reader.markSupported())
 			this.reader = new BufferedReader(reader);
@@ -77,11 +108,11 @@ public class DsvParser<T> {
 		this.dsvRecordParser = parser;
 	}
 	
-	abstract static class State {
+	private abstract static class State {
 		abstract State process(DsvParser<?> parser) throws IOException;
 	}
 
-	static class StartState extends State {
+	private static class StartState extends State {
 
 		State process(DsvParser<?> parser) throws IOException {
 			parser.reader.mark(1);
@@ -100,7 +131,7 @@ public class DsvParser<T> {
 		}
 	}
 
-	static class LineStartState extends State {
+	private static class LineStartState extends State {
 		State process(DsvParser<?> parser) throws IOException {
 			parser.reader.mark(1);
 			int character = parser.reader.read();
@@ -116,13 +147,13 @@ public class DsvParser<T> {
 		}
 	}
 
-	static class EndState extends State {
+	private static class EndState extends State {
 		State process(DsvParser<?> parser) {
 			return null;
 		}
 	}
 	
-	static class DelimiterState extends State {
+	private static class DelimiterState extends State {
 
 		State process(DsvParser<?> parser) throws IOException {
 			parser.reader.mark(1);
@@ -153,7 +184,7 @@ public class DsvParser<T> {
 		}
 	}
 	
-	static class LineFeedState extends State {
+	private static class LineFeedState extends State {
 		State process(DsvParser<?> parser) throws IOException {
 			parser.reader.mark(1);
 			int character = parser.reader.read();
@@ -169,7 +200,7 @@ public class DsvParser<T> {
 		}
 	}
 	
-	static class CarriageReturnState extends State {
+	private static class CarriageReturnState extends State {
 		State process(DsvParser<?> parser) throws IOException {
 			parser.reader.mark(1);
 			int character = parser.reader.read();
@@ -188,21 +219,21 @@ public class DsvParser<T> {
 		}
 	}
 	
-	static class StartStringFieldState extends State {
+	private static class StartStringFieldState extends State {
 		State process(DsvParser<?> parser) throws IOException {
 			parser.newField();
 			return new StringFieldState();
 		}
 	}
 	
-	static class StartFieldState extends State {
+	private static class StartFieldState extends State {
 		State process(DsvParser<?> parser) throws IOException {
 			parser.newField();
 			return new FieldState();
 		}
 	}
 	
-	static class StringFieldState extends State {
+	private static class StringFieldState extends State {
 		State process(DsvParser<?> parser) throws IOException {
 			int character = parser.reader.read();
 			if (character == EOF) {
@@ -244,7 +275,7 @@ public class DsvParser<T> {
 		}
 	}
 	
-	static class FieldState extends State {
+	private static class FieldState extends State {
 		State process(DsvParser<?> parser) throws IOException {
 			int character = parser.reader.read();
 			if (character == EOF) {
@@ -276,7 +307,7 @@ public class DsvParser<T> {
 		}
 	}
 	
-	static class EmbeddedStringState extends State {
+	private static class EmbeddedStringState extends State {
 		State process(DsvParser<?> parser) throws IOException {
 			int character = parser.reader.read();
 			if (character == EOF) {
@@ -303,6 +334,13 @@ public class DsvParser<T> {
 		this.fieldWriter.write(character);
 	}
 
+	/**
+	 * Once you have instantiated the parser and configured it (i.e. by setting callbacks),
+	 * you call this method to start the parsing.
+	 * 
+	 * @return
+	 * @throws IOException
+	 */
 	public List<T> readAll() throws IOException {
 		State s = new StartState();
 		while (!(s instanceof EndState)) {
@@ -328,12 +366,26 @@ public class DsvParser<T> {
 			lines.add(record);
 	}
 	
-	public DsvParser<?> useDelimiter(char delimeter) {
+	/**
+	 * Call this method to specify a delimiter different from a comma. Returns
+	 * the parser instance so you can do method chaining.
+	 * 
+	 * @param delimeter
+	 * @return
+	 */
+	public DsvParser<T> useDelimiter(char delimeter) {
 		this.delimeter = delimeter;
 		return this;
 	}
 	
-	public DsvParser<?> useQuoteCharacter(char quote) {
+	/**
+	 * Call this method to specify a different character for quoting strings with.
+	 * Returns the parser instance so you can do method chaining.
+	 * 
+	 * @param quote
+	 * @return
+	 */
+	public DsvParser<T> useQuoteCharacter(char quote) {
 		this.quote = quote;
 		return this;
 	}
